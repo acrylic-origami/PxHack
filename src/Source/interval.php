4948b6a3e713ab05df800686fa90e7a74ada6c98
Derek Lam <dc.lam@mail.utoronto.ca>
2018-12-18 20:59:50 -0500

    Start to populate a library of useful generators
    The base layer of HHx applications will be built on these sources
    before all the "sync-like" logic is heaped on top.

---
+++ src/Source/interval.php
@@ -0,0 +1,10 @@
+<?hh // strict
+namespace HHx\Source;
+use namespace HH\Asio;
+
+async function interval(int $delay): AsyncIterator<int> {
+	for($i = 0; ; $i++) {
+		await Asio\usleep($delay);
+		yield $i;
+	}
+}
