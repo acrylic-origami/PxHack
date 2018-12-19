ea5822547260115ee715d9b9bb12a8006fcf7c7e
Derek Lam <dc.lam@mail.utoronto.ca>
2018-12-18 21:02:03 -0500

    Force P2S to share
    The design intent is for `P2S` to receive only raw generators, so this
    cuts a lot of boilerplate otherwise needed to ensure all producers in the
    bottom layer are `sharable` between HHx operators.

--- src/Util/P2S.php
+++ src/Util/P2S.php
@@ -3,8 +3,9 @@ namespace HHx\Util;
 use HHx\{Supplier, const CANCEL};
 
 function P2S<T>(AsyncIterator<T> $P): Supplier<T> {
+	$S = share($P); // with other sharing ops (like publish) opinionating with `share` here is reasonable (and probably best)
 	return async $C ==> {
-		foreach($P await as $v) {
+		foreach($S await as $v) {
 			$cancel = await $C($v);
 			if($cancel !== null)
 				return CANCEL;
